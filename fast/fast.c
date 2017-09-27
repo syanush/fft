ca5e8390209910c88d6507ee5a37ef6d49fffd0d
Serguei Yanush <s.yanush@gmail.com>
2017-09-27 21:35:56 +0300

    2

--- fast/fast.c
+++ fast/fast.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-//#include <time.h>
 #include "fast.h"
 
 int reverse(int N, int n) {
@@ -14,13 +13,10 @@ int reverse(int N, int n) {
 /******************************************************************************/
 
 void fft_reverse(size_t N, double *re, double *im) {
-    double tmp;
-    int i;
     const size_t N2 = N >> 1;
     const int log2N = (int)log2(N);
 
-//#pragma omp parallel shared(re, im) private (i, tmp)
-//#pragma omp for
+#pragma omp parallel for
     for (int i = 0; i < N2; ++i) {
 
         //int reverted = reverse(N, i);
@@ -31,7 +27,7 @@ void fft_reverse(size_t N, double *re, double *im) {
         }
 
         /////
-        tmp = re[i];
+        double tmp = re[i];
         re[i] = re[reverted];
         re[reverted] = tmp;
         tmp = im[i];
@@ -59,25 +55,18 @@ int fft(const size_t N, double *re, double *im) {
     double phi = -M_2_PI / N;
 
     // предвычисление множителей
-#pragma omp parallel
-#pragma omp for
+#pragma omp parallel for
     for (size_t i = 0; i < N / 2; ++i) {
         double alpha = phi * i;
         wre[i] = cos(alpha);
         wim[i] = sin(alpha);
-
-        //wre[1] * wre[i - 1] - wim[1] * wim[i - 1];
-        //wim[i] = wre[1] * wim[i - 1] - wim[1] * wre[i - 1];
     }
-
-    //clock_t start = clock();
 
     fft_reverse(N, re, im);
 
     for (j = 0; j < log2N; ++j) {
 
-#pragma omp parallel
-#pragma omp for
+#pragma omp parallel for
         for (i = 0; i < N; ++i) {
             if (!(i & b)) {
                 int ib = i + b;
--- main.c
+++ main.c
@@ -3,8 +3,6 @@
 //
 
 #include <stdio.h>
-#include <time.h>
-
 #include "fast.h"
 
 #define bits 22
@@ -13,69 +11,22 @@
 double input_real[n]; // = {1, 2, 0, -1};
 double input_imag[n]; // = {0, -1, -1, 2};
 
-//#define M 40000000
-//double a[M];
-//
-//void test1() {
-//#pragma omp parallel for
-//    for(int i=0;i<M;i++) {
-//        a[i] = cos(sin(i));
-//    }
-//}
-
-
-//#define M 40000000
-//double a[M];
-//
-//void test1() {
-//#pragma omp parallel for
-//    for(int i=0;i<M;i++) {
-//        a[i] = cos(sin(i));
-//    }
-//}
-
-void tut1() {
-    int tsum=0;
-#pragma omp parallel
-    tsum += omp_get_thread_num();
-    printf("Sum is %d   ",tsum);
-}
-
-#define nmax 500000000
-void tut2() {
-    const long nn=500000000;
-    double dx = 1.0/nmax;
-    double sum = 0;
-
-    //int i;
-
-#pragma omp parallel for reduction(+:sum)
-    for(int i=0;i<nmax;++i) {
-        double x=i*dx;
-        sum += dx*sqrt(1-x*x);
-    }
-
-    printf("delta = %f    ", abs(M_PI/4-sum));
-}
-
 int main() {
 
     int error_code;
 
-    for (int t = 1; t <= 4; ++t) {
+    for (int t = 1; t <= 6; ++t) {
         omp_set_num_threads(t);
         printf("threads=%d\n", t);
-        for(int k=0;k<3;k++) {
+        for (int k = 0; k < 5; k++) {
 
+            //srand(time(NULL));
             srand(12345);
 
             for (int i = 0; i < n; ++i) {
                 input_real[i] = (double) rand() / RAND_MAX;
                 input_imag[i] = (double) rand() / RAND_MAX;
             }
-
-
-
 
             double wall_start = omp_get_wtime();
 
@@ -94,49 +45,6 @@ int main() {
         }
     }
 
-
-
-
-    //
-
-//    printf ( "  Number of processors available = %d\n", omp_get_num_procs () );
-//    printf ( "  Number of threads =              %d\n", omp_get_max_threads () );
-//    omp_set_num_threads(1);
-//#pragma omp parallel
-//    {
-//#pragma omp master
-//        printf("  Using %i threads.\n", omp_get_num_threads());
-//    }
-
-    //srand(time(NULL));
-
-//    for (int t = 1; t <= 4; ++t) {
-//        omp_set_num_threads(t);
-//        srand(12345);
-//
-//        for (int i = 0; i < n; ++i) {
-//            input_real[i] = (double) rand() / RAND_MAX;
-//            input_imag[i] = (double) rand() / RAND_MAX;
-//        }
-//
-//        clock_t start = clock();
-//
-//
-//        //test1();
-//
-//        clock_t end = clock();
-//        float seconds = (float) (end - start) / CLOCKS_PER_SEC;
-//
-//        printf("%f sec\n", seconds);
-//    }
-
-    //for(int i=0; i<n; ++i) {
-    //    printf("%8.2f %8.2f\n", input_real[i], input_imag[i]);
-    //}
-
-    //if (error_code) {
-    //    printf("Error code: %d\n", error_code);
-    //}
 
     return 0;
 }
